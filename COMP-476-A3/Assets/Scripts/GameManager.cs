2254a32e4633dc093d3dcabd2bb15969cb8f8818
christian-plourde <christian.bertin.plourde@gmail.com>
2020-03-16 11:51:36 -0400

    At Section 4 - Game Manager and Levels of PUN Doc

---
+++ COMP-476-A3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0); //load the launcher scene when we leave the room
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
